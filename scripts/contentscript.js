ff89f040889bb16af5f346bc457440bb898b77af
Alex <saez0724+github@googlemail.com>
2024-04-08 14:53:09 +0200

    shoppingbasketscript finished

--- scripts/contentscript.js
+++ scripts/contentscript.js
@@ -1,189 +1,147 @@
 let pizzaData = [
     {
         'pizza': 'Pizza Margherita, Pizza Salami, Pizza Mozzarella, Pizza Proscuitto',
-        'price': '8.90',
         'cut': 'geschnitten, ungeschnitten',
-    }
-];
-let pazzaVariant = [
-    {
         'size': 'Klein, Groß, XXL',
         'sizecm': '25, 32, 40',
         'price': '8.90, 12.90, 17.90',
         'extras': 'Ananas, Avocado, Bacon, Broccoli, Champignons, Garnelen',
         'extrasprice': '1.50',
-        'dough': '3.00'
+        'dough': '3.00',
+        'selectiontext': 'Wahl aus: Pizza geschnitten, Pizza ungeschnitten, mit Dinkel-Vollkornteig (mit Olivenöl), mit Ananas, mit Avocado und Mehr'
     }
 ];
 
 let shoppingBasket = [];
-
-let i = 0;
 let quantity = 1;
 
-function openDialog(pizzaName, pizzaPrice) {
+function render() {
+    let foodContent = document.getElementById('foodcontent');
+    foodContent.innerHTML = '';
 
-    document.getElementById('dialog').classList.remove('d_none');
-    let ingredientContent = document.getElementById('ingredientselection');
-    ingredientContent.innerHTML = '';
-    ingredientContent.innerHTML += /*html*/`
-        <button class="btn btn-outline-primary rounded-circle p-3 lh-1" id="close"
-                onclick="closeDialog()">X</button>
-            <div class="dialogheader" id="pizza">
-                <h3>${pizzaName}</h3>
-            </div>
-            <section class="ingredientsbox">
-                <div id="priceheader" class="priceheader">
-                    <h5>ab ${pizzaPrice} €</h5>
+    pizzaData.forEach(food => {
+        let pizzas = food['pizza'].split(', ');
+        let selectionText = food['selectiontext'];
+        let pizzaPrice = food['price'].split(", ")[0].trim().replace('.', ',');
+
+        pizzas.forEach((pizza, index) => {
+            foodContent.innerHTML += /*html*/`
+                <div onclick="openDialog(${index})" class="foodcontent">
+                    <h6><b>${pizza}</b></h6>
+                    <span>${selectionText}</span>
+                    <h6><b>ab ${pizzaPrice}</b></h6>
                 </div>
-                 <div id="ingredientsbox" class="ingredientselection">
-                    <span>Wähle deine Größe</span>
-                    <div class="custom-select">
-                        <select id="size">
-                            <option value="0">${pazzaVariant[0]['size'].split(', ')[0]} ø ${pazzaVariant[0]['sizecm'].split(', ')[0]} cm ${pazzaVariant[0]['price'].replace('.', ',').split(', ')[0]} €</option>
-                            <option value="1">${pazzaVariant[0]['size'].split(', ')[1]} ø ${pazzaVariant[0]['sizecm'].split(', ')[1]} cm ${pazzaVariant[0]['price'].replace('.', ',').split(', ')[1]} €</option>
-                            <option value="2">${pazzaVariant[0]['size'].split(', ')[2]} ø ${pazzaVariant[0]['sizecm'].split(', ')[2]} cm ${pazzaVariant[0]['price'].replace('.', ',').split(', ')[2]} €</option>
-                        </select>
-                    </div>
-                    <div>
-                        <span>Dein Zubereitungswusch</span>
-                        <div class="custom-select">
-                            <select id="preparation">
-                                <option value="0">Pizza ${pizzaData[0]['cut'].split(', ')[0]}</option>
-                                <option value="1">Pizza ${pizzaData[0]['cut'].split(', ')[1]}</option>
-                            </select>
-                        </div>
-                        <div>
-                            <span><b>Ihr Teig</b></span>
-                            <label  class="checkbox">mit Dinkel-Vollkornteig (mit Olivenöl) (+3,00 €)
-                                <input id="dough" type="checkbox">
-                                <span class="checkmark"></span>
-                            </label>
-                        </div>
-                        <div id="extrasCheckBox" >
-                        </div>
-                    </div>
-            </section>
-            <div class="dialogfooter">
-                <div class="dialogfootercontent">
-                    <svg id="plus" onclick="plusQuantity()" xmlns="http://www.w3.org/2000/svg" width="16"
-                        height="16" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
-                        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
-                        <path
-                            d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4" />
-                    </svg>
-                    <h3 id="quantity">1</h3>
-                    <svg id="minus" onclick="minusQuantity()" xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                        fill="currentColor" class="bi bi-dash-circle" viewBox="0 0 16 16">
-                        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
-                        <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8" />
-                    </svg>
-
-                    <div onclick="saveSelection()" class="dialogsum">${pazzaVariant[0]['price'].replace('.', ',').split(', ')[0]} €</div>
-                </div>
-            </div>
-    `;
-
-    generateExtrasCheckboxes();
-    document.getElementById('size').addEventListener('change', updatePrice);
-    document.getElementById('dough').addEventListener('change', updatePrice);
-
-    let extrasCheckboxes = document.querySelectorAll('.extras');
-    extrasCheckboxes.forEach(checkbox => {
-        checkbox.addEventListener('change', updatePrice);
+            `;
+        });
     });
 };
 
-function updatePrice() {
-    let sizeIndex = document.getElementById('size').value;
-    let doughCheckbox = document.getElementById('dough');
+function openDialog(index) {
+    let selectedPizza = getPizzaByIndex(index);
+    loadContentToDialog(selectedPizza);
+    document.getElementById('dialog').classList.remove('d_none');
+};
 
-    let extrasPrice = 0;
-
-    let extrasCheckboxes = document.querySelectorAll('.extras');
-    extrasCheckboxes.forEach(checkbox => {
-        if (checkbox.checked) {
-            extrasPrice += 1.50;
+function getPizzaByIndex(index) {
+    let pizzaIndex = 0;
+    for (let i = 0; i < pizzaData.length; i++) {
+        let pizzas = pizzaData[i]['pizza'].split(', ');
+        if (index < pizzaIndex + pizzas.length) {
+            return {
+                name: pizzas[index - pizzaIndex],
+                price: pizzaData[i]['price'].split(", ")[0].trim().replace('.', ',')
+            };
         }
-    });
+        pizzaIndex += pizzas.length;
+    }
+};
 
-    let doughPrice = doughCheckbox.checked ? 3.00 : 0;
+function loadContentToDialog(pizza) {
+    document.getElementById('pizzaname').innerText = pizza.name;
+    document.getElementById('priceheader').innerText = "ab " + pizza.price + " €";
+    let sizeOptionsHTML = '';
+    let sizes = pizzaData[0]['size'].split(', ');
+    let sizeCMs = pizzaData[0]['sizecm'].split(', ');
+    let prices = pizzaData[0]['price'].split(', ');
 
-    let price = parseFloat(pazzaVariant[0]['price'].split(', ')[sizeIndex]);
+    for (let i = 0; i < sizes.length; i++) {
+        sizeOptionsHTML += `
+            <option value="${i}">${sizes[i]} ø ${sizeCMs[i]} cm ${prices[i].replace('.', ',')} €</option>
+        `;
+    }
+    document.getElementById('sizeselection').innerHTML = sizeOptionsHTML;
 
-    let totalPrice = (price + doughPrice + extrasPrice) * quantity;
+    let preparationOptionsHTML = '';
+    let cuts = pizzaData[0]['cut'].split(', ');
+    for (let i = 0; i < cuts.length; i++) {
+        preparationOptionsHTML += `
+            <option value="${i}">Pizza ${cuts[i]}</option>
+        `;
+    }
+    document.getElementById('preparation').innerHTML = preparationOptionsHTML;
+    generateExtrasCheckboxes();
+    updatePrice();
+};
 
-    document.querySelector('.dialogsum').textContent = totalPrice.toFixed(2).replace('.', ',') + " €";
-}
+function closeDialogAndSave() {
+    saveSelection();
+    renderBasket();
+    closeDialog();
+};
+
+function closeDialog() {
+    document.getElementById('dialog').classList.add('d_none');
+};
 
 function generateExtrasCheckboxes() {
     let extrasCheckBox = document.getElementById('extrasCheckBox');
-    extrasCheckBox.innerHTML = '<b>Ihre Extras + 1,50 €</b>';
-
-    let extra = pazzaVariant[0];
-
-    let extrasArray = extra['extras'].split(', ');
+    extrasCheckBox.innerHTML = '';
+    let selectedPizza = pizzaData[0];
+    let extrasArray = selectedPizza['extras'].split(', ');
 
     for (let i = 0; i < extrasArray.length; i++) {
         extrasCheckBox.innerHTML += /*html*/`
-            <label class="checkbox">${extrasArray[i]}
-                <input class="extras" type="checkbox">
+            <label ${i} class="checkbox">${extrasArray[i]}
+                <input ${i} id="extrasCheck" class="extras" type="checkbox">
                 <span class="checkmark"></span>
             </label>
         `;
     }
 };
 
-function closeDialogAndSave() {
-    saveSelection(); // Speichern der aktuellen Auswahl
-    closeDialog(); // Dialog schließen
-}
+function updatePrice() {
+    let selectedSizeIndex = document.getElementById('sizeselection').value;
+    let selectedPrice = parseFloat(pizzaData[0]['price'].split(', ')[selectedSizeIndex]);
 
-function closeDialog() {
-    document.getElementById('dialog').classList.add('d_none');
-};
-
-function render() {
-    let foodContent = document.getElementById('foodcontent');
-    let ingredientContent = document.getElementById('ingredientselection');
-    foodContent.innerHTML = '';
-    ingredientContent.innerHTML = '';
-
-
-    for (let i = 0; i < pizzaData.length; i++) {
-        let food = pizzaData[i];
-        let pizzas = food['pizza'].split(', ');
-
-        for (let j = 0; j < pizzas.length; j++) {
-            let pizzaName = pizzas[j].trim();
-            let pizzaPrice = food['price'].trim().replace('.', ',');
-            foodContent.innerHTML += /*html*/`
-                <div onclick="openDialog('${pizzaName}', '${pizzaPrice}')" class="foodcontent">
-                <h6><b>${pizzaName}</b></h6>
-                <span>Wahl aus: Pizza geschnitten, Pizza ungeschnitten, mit Dinkel-Vollkornteig (mit Olivenöl), mit
-                    Ananas, mit Avocado
-                    und Mehr</span>
-                <h6><b>ab ${pizzaPrice} €</b></h6></div>
-        `;
-            ingredientContent.innerHTML += /*html*/`
-            `;
+    let doughCheckbox = document.getElementById('dough');
+    let doughPrice = 0;
+    if (doughCheckbox.checked) {
+        doughPrice = parseFloat(pizzaData[0]['dough']);
+    }
+    let extrasCheckboxes = document.getElementsByClassName('extras');
+    let extrasPrice = 0;
+    for (let i = 0; i < extrasCheckboxes.length; i++) {
+        if (extrasCheckboxes[i].checked) {
+            extrasPrice += parseFloat(pizzaData[0]['extrasprice']);
         }
     }
+
+    let totalPrice = (selectedPrice + doughPrice + extrasPrice) * quantity;
+    document.getElementById('selectedPrice').innerText = totalPrice.toFixed(2).replace('.', ',') + " €";
 };
 
 function plusQuantity() {
     if (quantity < 5) {
         quantity++;
-        updatePrice()
+        updatePrice();
         document.getElementById('quantity').textContent = quantity;
     }
 };
 
-
 function minusQuantity() {
     if (quantity > 1) {
         quantity--;
-        updatePrice()
+        updatePrice();
         document.getElementById('quantity').textContent = quantity;
     }
 };
@@ -194,38 +152,33 @@ function changeQuantity(newQuantity) {
 };
 
 function saveSelection() {
-    let selectedPizza = document.getElementById('pizza').innerText;
-    let selectedSizeIndex = document.getElementById('size').selectedIndex;
-    let selectedSizeText = document.getElementById('size').options[selectedSizeIndex].text;
-    let selectedSize = selectedSizeText.split(" ")[0];
-    let selectedCutIndex = document.getElementById('preparation').selectedIndex;
-    let selectedCut = document.getElementById('preparation').options[selectedCutIndex].text;
+    let selectedPizza = document.getElementById('pizzaname').innerText;
+    let selectedSizeIndex = document.getElementById('sizeselection').value;
+    let selectedSize = pizzaData[0]['size'].split(', ')[selectedSizeIndex];
     let selectedDough = document.getElementById('dough').checked ? 'mit Dinkel-Vollkornteig (mit Olivenöl)' : '';
-    let selectedPrice = null;
-    let selectedQuantity = document.getElementById('quantity').textContent;
-
-    let selectedPriceElement = document.querySelector('.dialogsum');
-    if (selectedPriceElement) {
-        selectedPrice = selectedPriceElement.textContent;
-    }
-
+    let selectedPreparationIndex = document.getElementById('preparation').value;
+    let selectedPreparation = pizzaData[0]['cut'].split(', ')[selectedPreparationIndex];
+    let shoppingBasket = JSON.parse(localStorage.getItem('shoppingBasket')) || [];
+    let selectedPrice = document.getElementById('selectedPrice').innerText;
+    let selectedQuantity = document.getElementById('quantity').innerText;
+    let selectedSizeCmIndex = document.getElementById('sizeselection').value;
+    let selectedSizeCm = pizzaData[0]['sizecm'].split(', ')[selectedSizeCmIndex];
     let selectedExtras = [];
-    let extrasCheckboxes = document.querySelectorAll('.extras:checked');
-    extrasCheckboxes.forEach(checkbox => {
-        let labelText = checkbox.parentNode.textContent.trim();
-        selectedExtras.push(labelText.replace(/\n/g, '').trim());
+
+    document.querySelectorAll('.extras').forEach(checkbox => {
+        if (checkbox.checked) {
+            selectedExtras.push(checkbox.parentElement.innerText.trim());
+        }
     });
-
-    let selectedExtrasString = selectedExtras.join(' ');
-
     let selection = {
         pizza: selectedPizza,
         size: selectedSize,
-        cut: selectedCut,
         dough: selectedDough,
+        preporation: selectedPreparation,
+        price: selectedPrice,
         extras: selectedExtras,
-        sum: selectedPrice,
-        quantity: selectedQuantity
+        quantity: selectedQuantity,
+        sizeCm: selectedSizeCm
     };
 
     shoppingBasket.push(selection);
@@ -233,24 +186,20 @@ function saveSelection() {
     localStorage.setItem('shoppingBasket', JSON.stringify(shoppingBasket));
 
     resetDialog();
-}
-
+};
 
 function resetDialog() {
     document.getElementById('dialog').classList.add('d_none');
-
-    document.getElementById('size').selectedIndex = 0;
+    document.getElementById('sizeselection').selectedIndex = 0;
     document.getElementById('preparation').selectedIndex = 0;
+    document.getElementById('dough').checked = false;
 
-    document.querySelectorAll('.extras').forEach(checkbox => {
+    document.querySelectorAll('extras').forEach(checkbox => {
         checkbox.checked = false;
     });
 
     quantity = 1;
     document.getElementById('quantity').textContent = '1';
-
-    let defaultPrice = pazzaVariant[0]['price'].replace('.', ',').split(', ')[0];
-    document.querySelector('.dialogsum').textContent = defaultPrice + " €";
-}
+};
 
 
--- scripts/shoppingbasket.js
+++ scripts/shoppingbasket.js
@@ -1,71 +1,110 @@
 
-let basketIndex = 0;
-
+let storageBasket = JSON.parse(localStorage.getItem("shoppingBasket")) || [];
+let i = 0;
+let lotalPrice = 0;
+let deliveryPricePerItem = 0.90;
 
 function renderBasket() {
     let ingredientContent = document.getElementById('basketproduct');
     ingredientContent.innerHTML = '';
 
-    if (localStorage.length === 0) {
-        ingredientContent.innerHTML = '<p>Der Einkaufswagen ist leer.</p>';
-        return;
+    for (let i = 0; i < storageBasket.length; i++) {
+        let basket = storageBasket[i];
+
+        ingredientContent.innerHTML += /*html*/`
+            <div class="products"> 
+                <span><u><b>${basket['pizza']}</b></u> </span><b id="productprice_${i}">${basket['price']}</b>
+            </div>
+            <div class="productsdescription"><span>${basket['size']} ø ${basket['sizeCm']} cm ${basket['preporation']} ${basket['dough']} ${basket['extras'].join(', ')}</span>
+            </div>
+            <div class="addproduct">
+                <svg onclick="plusQuantityBasket(${i})" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
+                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
+                    <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4" />
+                </svg>
+                <h4 id=quantityBasket>${basket['quantity']}</h4>
+                <svg onclick="minusQuantityBasket(${i})" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-dash-circle" viewBox="0 0 16 16">
+                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
+                    <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8" />
+                </svg>
+            </div>
+        `;
+    }
+    loadBasket();
+    updateBasketPrice();
+};
+
+function loadBasket() {
+    let storageBasket = localStorage.getItem("shoppingbasket");
+    if (storageBasket) {
+        shoppingBasket = JSON.parse(storageBasket);
+    }
+};
+
+function updateLocalStorage() {
+    localStorage.setItem('shoppingBasket', JSON.stringify(storageBasket));
+};
+
+function plusQuantityBasket(i) {
+    let basket = storageBasket[i];
+    if (basket.quantity < 5) {
+        basket.quantity++;
+        updateLocalStorage();
+        updateBasketPrice();
+        renderBasket();
+    }
+};
+
+function minusQuantityBasket(i) {
+    let basket = storageBasket[i];
+    if (basket.quantity > 1) {
+        basket.quantity--;
+        updateLocalStorage();
+        updateBasketPrice();
     } else {
+        (basket.quantity < 1)
+        deleteProduct();
+    }
+    renderBasket();
+};
 
-        for (let i = 0; i < localStorage.length; i++) {
-            for (let i = 0; i < localStorage.length; i++) {
+function deleteProduct(index) {
+    storageBasket.splice(index, 1);
+    localStorage.setItem('shoppingBasket', JSON.stringify(storageBasket));
+    renderBasket();
+};
 
-                let basket = JSON.parse(localStorage.getItem('shoppingBasket'))[i];
+function updateBasketPrice() {
+    let basketPrice = 0;
 
-                ingredientContent.innerHTML += /*html*/`
-        <div id class="products">
-            <span>${basket['quantity']}</span>
-            <span><u><b>${basket['pizza']}</b></u> ${basket['size']}</span>${basket['sum']}<span></span>
-        </div>
-        <div class="productsdescription"><span>${basket['cut']}, ${basket['dough']}, ${basket['extras']}</span>
-        </div>
-        <div class="addproduct">
-            <span><u>Anmerkung<br>hinzufügen</u></span>
-            <svg onclick="plusQuantityBasket()" id="plusbasket" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                class="bi bi-plus-circle" viewBox="0 0 16 16">
-                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
-                <path
-                    d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4" />
-            </svg>
-            <h3 id="quantitybasket">1</h3>
-            <svg onclick="minusQuantityBasket()" id="minusbasket" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                class="bi bi-dash-circle" viewBox="0 0 16 16">
-                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
-                <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8" />
-            </svg>
-        </div>
-    `;
-            }
+    for (let i = 0; i < storageBasket.length; i++) {
+        let basket = storageBasket[i];
+        let productPrice = parseFloat(basket['price'].replace(',', '.'));
+        let quantity = basket['quantity'];
+        let totalPriceForProduct = productPrice * quantity;
+        let productPriceElement = document.getElementById(`productprice_${i}`);
+        if (productPriceElement) {
+            productPriceElement.innerHTML = `${totalPriceForProduct.toFixed(2).replace('.', ',')} €`;
         }
+        basketPrice += totalPriceForProduct;
     }
-};
+    let deliveryPrice = deliveryPricePerItem;
+    let totalPrice = basketPrice + deliveryPrice;
+    let produktPrice = document.getElementById('basketprice');
+    produktPrice.innerHTML = `${basketPrice.toFixed(2).replace('.', ',')} €`;
 
-function plusQuantityBasket() {
-    if (quantity < 5) {
-        quantity++;
+    let deliveryContent = document.getElementById('deliveryprice');
+    deliveryContent.innerHTML = `${deliveryPrice.toFixed(2).replace('.', ',')} €`;
 
-        document.getElementById('quantitybasket').textContent = quantity;
-        localStorage.setItem('shoppingBasket', JSON.stringify(shoppingBasket))[i];
+    let endPrice = document.getElementById('totalprice');
+    if (endPrice) {
+        endPrice.innerHTML = `${totalPrice.toFixed(2).replace('.', ',')} €`;
     }
-};
 
-
-function minusQuantityBasket() {
-    if (quantity > 1) {
-        quantity--;
-
-        document.getElementById('quantitybasket').textContent = quantity;
-        localStorage.setItem('shoppingBasket', JSON.stringify(shoppingBasket));
+    let checkoutElement = document.getElementById('checkout');
+    if (checkoutElement) {
+        checkoutElement.innerHTML = `${totalPrice.toFixed(2).replace('.', ',')} €`;
     }
-};
-
-function changeQuantityBasket(newQuantity) {
-    quantity = newQuantity;
-    document.getElementById('quantitybasket').textContent = quantity;
 };
 
 
