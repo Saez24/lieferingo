6576ad4486a400a0cc52c14ce85ad04109869d77
Alex <saez0724+github@googlemail.com>
2024-04-09 07:37:40 +0200

    working on shoppingbasket

--- scripts/shoppingbasket.js
+++ scripts/shoppingbasket.js
@@ -5,34 +5,40 @@ let lotalPrice = 0;
 let deliveryPricePerItem = 0.90;
 
 function renderBasket() {
-    let ingredientContent = document.getElementById('basketproduct');
-    ingredientContent.innerHTML = '';
+    if (storageBasket.length === 0) {
+        let shoppingbasketContent = document.getElementById('shoppingbasket');
+        shoppingbasketContent.innerText = 'Bitte füllen Sie den Warenkorb.';
+    } else {
+        let ingredientContent = document.getElementById('basketproduct');
+        ingredientContent.innerHTML = '';
+        for (let i = 0; i < storageBasket.length; i++) {
+            let basket = storageBasket[i];
 
-    for (let i = 0; i < storageBasket.length; i++) {
-        let basket = storageBasket[i];
-
-        ingredientContent.innerHTML += /*html*/`
-            <div class="products"> 
-                <span><u><b>${basket['pizza']}</b></u> </span><b id="productprice_${i}">${basket['price']}</b>
-            </div>
-            <div class="productsdescription"><span>${basket['size']} ø ${basket['sizeCm']} cm ${basket['preporation']} ${basket['dough']} ${basket['extras'].join(', ')}</span>
-            </div>
-            <div class="addproduct">
-                <svg onclick="plusQuantityBasket(${i})" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
-                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
-                    <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4" />
-                </svg>
-                <h4 id=quantityBasket>${basket['quantity']}</h4>
-                <svg onclick="minusQuantityBasket(${i})" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-dash-circle" viewBox="0 0 16 16">
-                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
-                    <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8" />
-                </svg>
-            </div>
-        `;
+            ingredientContent.innerHTML += /*html*/`
+                <div class="products"> 
+                    <span><u><b>${basket['pizza']}</b></u> </span><b id="productprice_${i}">${basket['price']}</b>
+                </div>
+                <div class="productsdescription"><span>${basket['size']} ø ${basket['sizeCm']} cm ${basket['preporation']} ${basket['dough']} ${basket['extras'].join(', ')}</span>
+                </div>
+                <div class="addproduct">
+                    <svg onclick="plusQuantityBasket(${i})" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
+                        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
+                        <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4" />
+                    </svg>
+                    <h4 id=quantityBasket>${basket['quantity']}</h4>
+                    <svg onclick="minusQuantityBasket(${i})" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-dash-circle" viewBox="0 0 16 16">
+                        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
+                        <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8" />
+                    </svg>
+                </div>
+            `;
+        }
+        loadBasket();
+        updateBasketPrice();
     }
-    loadBasket();
-    updateBasketPrice();
 };
+
+
 
 function loadBasket() {
     let storageBasket = localStorage.getItem("shoppingbasket");
